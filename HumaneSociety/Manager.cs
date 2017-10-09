ab068cb1511ce73d26366fd00e53a4d563be801f
carlosv123 <vasquezcarlos1996@gmail.com>
2017-10-09 10:00:09 -0700

    Checking adopter choice

--- HumaneSociety/Manager.cs
+++ HumaneSociety/Manager.cs
@@ -17,19 +17,35 @@ namespace HumaneSociety
 
         public void EntryMenu()
         {
-            Console.WriteLine("Are you an adopter or employee> \n 1: adopter \n 2:Employee");
+            Console.WriteLine("Are you an adopter or employee \n 1: adopter \n 2:Employee");
             string choice = Console.ReadLine();
 
             switch(choice)
             {
                 case "1":
-                    ChechAdopter();
+                    CheckAdopter();
                     break;
                 case "2":
                     CheckEmployee();
                     break;
             }
         }
+        public void CheckAdopter()
+        {
+            Console.WriteLine("Are you a new adopter or an exsisting adopter \n 1: new adopter \n 2: I'm an exsisting adopter");
+            string choice = Console.ReadLine();
 
+            switch (choice)
+            {
+                case "1":
+                    GetNewAdopter();
+                    break;
+                case "2":
+                    GetExsistingAdopter();
+                    break;
+            }
+        }
+        
+      
     }
 }
